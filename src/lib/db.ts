48f3cb5775aafaa22c6c840837311cb08f6b360c
derafli10 <raflizaardiansa10@gmail.com>
2025-05-05 21:54:35 +0700

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,10 +2,7 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host:     process.env.MYSQL_HOST,
-  user:     process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  process.env.DATABASE_URL!,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
