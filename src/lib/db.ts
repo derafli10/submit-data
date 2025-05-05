09d1283fad0e4651a8f83258c76f37c3da346c71
derafli10 <raflizaardiansa10@gmail.com>
2025-05-05 21:57:40 +0700

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,4 @@
-// src/lib/db.ts
 import mysql from 'mysql2/promise';
 
-const pool = mysql.createPool({
-  process.env.DATABASE_URL!,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
-
+const pool = mysql.createPool(process.env.DATABASE_URL!);
 export default pool;
